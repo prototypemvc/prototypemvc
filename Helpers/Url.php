e1562027de849bac064e1eb7d22e29cad5a0099f
Rick van der Wolk <rickvanderwolk@gmail.com>
2015-06-26 00:55:38 +0200

    Changed Helpers\Url
    Changed redirect, removed unnecessary ’/‘

--- Helpers/Url.php
+++ Helpers/Url.php
@@ -7,7 +7,7 @@ use \Pmvc\Helpers\Session;
 class Url {
 
     public static function redirect($url = null) {
-        header('location: ' . BASE_URL . '/' . $url);
+        header('location: ' . BASE_URL . $url);
         exit;
     }
 
