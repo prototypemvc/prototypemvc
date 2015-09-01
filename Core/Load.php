aa922bb0642a4fb10599e631b7096281b5b0978c
Rick van der Wolk <rickvanderwolk@gmail.com>
2015-09-01 15:56:18 +0200

    documentation Load

--- Core/Load.php
+++ Core/Load.php
@@ -8,26 +8,55 @@ use \Pmvc\Core\Model;
 
 class Load {
 
+    /**
+    * Load a given CSS file. 
+    * @param string path to file  
+    * @return boolean 
+    */
     public static function css($file = false) {
 
         return View::css($file);
     }
 
+    /**
+    * Get file content. 
+    * @param string path to file  
+    * @return boolean 
+    */
     public static function file($file = false) {
 
         return File::get($file);
     }
 
+    /**
+    * Load a given JavaScript file. 
+    * @param string path to file  
+    * @return boolean 
+    */
     public static function js($file = false) {
 
         return View::js($file);
     }
 
+    /**
+    * Load a given model. 
+    * @param string path to file  
+    * @example model('blog/model/blog') 
+    * @example model('blog/model/blog.php') 
+    * @return boolean 
+    */
     public static function model($file = false) {
 
         return Model::load($file);
     }
 
+    /**
+    * Load a given view. 
+    * @param string path to file  
+    * @example model('blog/view/index') 
+    * @example model('blog/view/index.php') 
+    * @return boolean 
+    */
     public static function view($file = false) {
 
         return View::load($file);
