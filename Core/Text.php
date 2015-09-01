c5550e880c635e2b4a3f4a09d3a8c0ba8f7883f4
Rick van der Wolk <rickvanderwolk@gmail.com>
2015-09-01 16:49:09 +0200

    documentation Text

--- Core/Text.php
+++ Core/Text.php
@@ -4,6 +4,12 @@ namespace Pmvc\Core;
 
 class Text {
 
+    /** 
+    * Check if the given string contains a specific substring. 
+    * @param string full string 
+    * @param string substring 
+    * @return boolean 
+    */
     public static function contains($string = false, $substring = false) {
 
         if ($substring && $substring && strpos($string, $substring) !== false) {
@@ -14,6 +20,15 @@ class Text {
         return false;
     }
 
+    /** 
+    * Get string length, number of words or the occurences of specific word. 
+    * @param string full string 
+    * @param string selector 
+    * @example count('hello world') - will return length of string which is 11 
+    * @example count('hello world', 'words') - will return number of words which is 2 
+    * @example count('hello world', ':hello') - will return number of occurences of string 'hello' which is 1 
+    * @return boolean 
+    */
     public static function count($string = false, $selector = false) {
 
         if ($string) {
@@ -32,6 +47,11 @@ class Text {
         return false;
     }
 
+    /** 
+    * Get hashed string.  
+    * @param string full string 
+    * @return string hashed string  
+    */
     public static function hash($string = false) {
 
         if($string) {
@@ -42,6 +62,11 @@ class Text {
         return false;
     }
 
+    /** 
+    * Get string length. 
+    * @param string full string 
+    * @return int length of string 
+    */ 
     public static function length($string = false) {
 
         if ($string) {
@@ -52,6 +77,11 @@ class Text {
         return false;
     }
 
+    /** 
+    * Get string in lowercase.  
+    * @param string full string 
+    * @return string in lowercase 
+    */ 
     public static function lowercase($string = false, $selector = false) {
 
         if ($string) {
@@ -69,6 +99,13 @@ class Text {
         return false;
     }
 
+    /** 
+    * Get string with specific substring replaced. 
+    * @param string full string  
+    * @param string substring to find and replace 
+    * @param string replace with this value 
+    * @return string trimmed 
+    */ 
     public static function replace($string = false, $find = false, $replaceWith = '') {
 
         if ($string && $find) {
@@ -79,6 +116,11 @@ class Text {
         return false;
     }
 
+    /** 
+    * Get reversed string.   
+    * @param string full string  
+    * @return string reversed  
+    */ 
     public static function reverse($string = false) {
 
         if ($string) {
@@ -90,6 +132,11 @@ class Text {
         return false;
     }
 
+    /** 
+    * Get string shuffled.    
+    * @param string full string  
+    * @return shuffeld string 
+    */ 
     public static function shuffle($string = false) {
 
         if ($string) {
@@ -100,6 +147,11 @@ class Text {
         return false;
     }
 
+    /** 
+    * Get trimmed string.   
+    * @param string full string  
+    * @return string trimmed 
+    */ 
     public static function strip($string = false) {
 
         if ($string) {
@@ -110,7 +162,13 @@ class Text {
         return false;
     }
 
-    // Stephen Watkins - stackoverflow
+    /** 
+    * Get a random string.  
+    * Thanks Stephen Watkins : stackoverflow
+    * @param int string legnth 
+    * @param string allowed characters 
+    * @return string generated random string 
+    */ 
     public static function random($length = 10, $allowedCharacters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') {
 
         $charactersLength = self::length($allowedCharacters);
@@ -121,8 +179,15 @@ class Text {
         return $randomString;
     }
 
-    // Charalambos Tsangarides - https://blog.ergatides.com
-    // LET OP is nog gevoelig voor geen internet of error tijdens laden, moet afgehandeld worden
+    /** 
+    * Get lorem ipsum text. 
+    * Thanks Charalambos Tsangarides : https://blog.ergatides.com 
+    * WARNING! Needs an internet connection. 
+    * @param int number of 'what' 
+    * @param string what 
+    * @param int start with lorem ipsum? 
+    * @return string lorem ipsum text  
+    */ 
     public static function loremIpsum($amount = 3, $what = 'paras', $startWithLoremIpsum = 0) {
 
         $lipsum = simplexml_load_file("http://www.lipsum.com/feed/xml?amount=$amount&what=$what&start=$startWithLoremIpsum")->lipsum;
@@ -135,6 +200,13 @@ class Text {
         return 'Random text could not be loaded...';
     }
 
+    /** 
+    * Get substring.  
+    * @param string full string 
+    * @param int first index (starting point)
+    * @param int max string length 
+    * @return string substring 
+    */ 
     public static function substring($string = false, $firstIndex = 0, $maxStringLength = false) {
 
         if ($string) {
@@ -150,6 +222,11 @@ class Text {
         return false;
     }
 
+    /** 
+    * Get string in uppercase.  
+    * @param string full string 
+    * @return string in uppercase 
+    */ 
     public static function uppercase($string = false, $selector = false) {
 
         if ($string) {
