fd36ea7bbcabbdd40c1921e11382c903d59c1152
Rick van der Wolk <rickvanderwolk@gmail.com>
2015-09-03 18:26:17 +0200

    documentation Date difference

--- Helpers/Date.php
+++ Helpers/Date.php
@@ -6,6 +6,20 @@ use \DateTime;
 
 class Date {
 
+    /** 
+    * Get time difference between two dates. If no second date is given, 
+    * the first date will be .... against the current date. 
+    * @param string first date 
+    * @param string second date 
+    * @param string format 
+    * @example difference('2015-01-01') - will return the difference in seconds 
+    * between 1st of january 2015 and now 
+    * @example difference('2015-01-01', '2015-01-02') - will return difference 
+    * in seconds between 1st of january 2015 and 2nd of january 2015
+    * @example difference('2015-01-01', '2015-01-02', 'days') - will return difference 
+    * in hours  between 1st of january 2015 and 2nd of january 2015
+    * @return int difference in chosen format, seconds by default 
+    */ 
     public static function difference($date1 = false, $date2 = false, $format = 'seconds') {
 
         if (!$date1 || !self::isValid($date1)) {
@@ -50,7 +64,7 @@ class Date {
             return $total;
         }
     }
-
+    
     public static function format($date = false, $format = 'Y-m-d H:i:s') {
 
         if($data) {
